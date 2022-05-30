26198a2b3af68a0bb5768c10e97628ca3495d180
EnderFlame7 <73864655+EnderFlame7@users.noreply.github.com>
2022-05-30 15:04:06 -0700

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,11 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+	
+	// Sets collision boolean to false by default
+	public BufferedImage image;
+	public boolean collision = false;
+
+}
